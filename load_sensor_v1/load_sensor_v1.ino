139b12cb72bfd7f37a36d5923ef808c333bf16e2
Dusan Micic <Dusan Micic>
2016-03-06 14:27:07 -0800

    t latest commit

--- load_sensor_v1/load_sensor_v1.ino
+++ load_sensor_v1/load_sensor_v1.ino
@@ -1,34 +1,37 @@
+int analogPin = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3
+                       // outside leads to ground and +5V
+int val1 = 0;
+int val = 0;           // variable to store the value read
+int val2 = 0;
 
-int sensorValue1 = 0;
-int sensorValue2 = 0;
 
-void setup() {
-  // put your setup code here, to run once:
- Serial.begin(9600);
- pinMode(9,OUTPUT);
- 
- digitalWrite(9,HIGH);
-  delay(1000);
-  digitalWrite(9,LOW);
-  delay(1000);
- digitalWrite(9,HIGH);
-  delay(1000);
-  digitalWrite(9,LOW);
-  delay(1000);
-
+void setup()
+{
+  Serial.begin(9600);          //  setup serial
 }
 
 
-void loop() {
-sensorValue1 = analogRead(A0);
-sensorValue2 = analogRead(A1);
-float voltageDif=sensorValue1-sensorValue2;
 
-Serial.print(sensorValue1);
-Serial.print("\t");
-Serial.print(sensorValue2);
-Serial.print("\t");
-Serial.print(voltageDif);
-Serial.print("\n");
-delay(200);
+void loop()
+
+{
+  val1 = analogRead(analogPin);    // read the input pin
+  if(val1 > 700)
+  {
+    val2 = val1;
+    Serial.print(val1);
+    Serial.print(" ");
+  }
+
+  else
+  {
+    val = analogRead(analogPin);
+    val2 = val;
+    Serial.print(val);
+    Serial.print(" ");
+  }
+  
+  Serial.print((val2-val)*1.13);             // debug value
+  Serial.print(" lbs\n");
+  delay(250);
 }
--- sketch_feb03a/sketch_feb03a.ino
+++ sketch_feb03a/sketch_feb03a.ino
@@ -1,15 +1,34 @@
-int sensorPin = A0;
-float sensorValue = 0;
+
+int sensorValue1 = 0;
+int sensorValue2 = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+ Serial.begin(9600);
+ pinMode(9,OUTPUT);
+ 
+ digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(9,LOW);
+  delay(1000);
+ digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(9,LOW);
+  delay(1000);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  sensorValue = analogRead(sensorPin);
 
-  Serial.println(sensorValue);
-  delay(100);
+void loop() {
+sensorValue1 = analogRead(A0);
+sensorValue2 = analogRead(A1);
+float voltageDif=sensorValue1-sensorValue2;
+
+Serial.print(sensorValue1);
+Serial.print("\t");
+Serial.print(sensorValue2);
+Serial.print("\t");
+Serial.print(voltageDif);
+Serial.print("\n");
+delay(200);
 }
