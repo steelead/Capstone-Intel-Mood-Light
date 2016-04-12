4c2fa59540986bf5ebb2697c420075892d94ad31
steelead <steelead@pdx.edu>
2016-04-12 12:00:25 -0700

    code for merging LED Strip code and Weight sensor

---
+++ weightAndLEDStrip/weightAndLEDStrip.ino
@@ -0,0 +1,73 @@
+int analogPin = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3
+                       // outside leads to ground and +5V
+int val1 = 0;
+int val = 0;           // variable to store the value read
+int val2 = 0;
+#define REDPIN 9
+#define GREENPIN 10
+#define BLUEPIN 11 
+#define PERSON 8
+int read1 = 0;
+int read2 = 0;
+int read3 = 0;
+int read4 = 0;
+int lastVal = 0;
+
+
+
+void setup()
+{
+  Serial.begin(9600);          //  setup serial
+  pinMode(PERSON, INPUT);
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  analogWrite(REDPIN, 255);
+  analogWrite(GREENPIN, 255);
+  analogWrite(BLUEPIN, 255);
+}
+
+
+
+void loop()
+{
+  if(digitalRead(PERSON) == HIGH)
+  {
+    Serial.print("HIGH ");
+    val1 = analogRead(analogPin);
+    val2 = val1;
+  }
+  else
+  {
+    Serial.print("LOW ");
+    delay(2000);
+    val2 = analogRead(analogPin);
+    lastVal = (val2-val1)*1.163;
+  }
+
+  if (lastVal < 170 && lastVal > 130)
+  {
+    analogWrite(REDPIN, 255);
+    analogWrite(GREENPIN, 0);
+    analogWrite(BLUEPIN, 0);
+  }
+  else if (lastVal < 120 && lastVal > 80)
+  {
+    analogWrite(REDPIN, 0);
+    analogWrite(GREENPIN, 255);
+    analogWrite(BLUEPIN, 0);
+  }
+  else
+  {
+    analogWrite(REDPIN, 0);
+    analogWrite(GREENPIN, 255);
+    analogWrite(BLUEPIN, 0);
+  }
+
+  Serial.print(lastVal);
+  Serial.print(' ');
+  Serial.print(val2);
+  Serial.print(' ');
+  Serial.print((val2-val1)*1.163);             // debug value
+  Serial.print(" lbs\n");
+}
